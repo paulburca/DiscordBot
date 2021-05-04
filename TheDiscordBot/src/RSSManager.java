6c853d3d8900a03f695c02a2c98f6d13d02c5924
Paul <gigi.boss52@gmail.com>
2021-05-04 14:45:49 +0300

    Update RSSManager.java

--- TheDiscordBot/src/RSSManager.java
+++ TheDiscordBot/src/RSSManager.java
@@ -1,41 +1,70 @@
+import com.sun.syndication.feed.module.Module;
+import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
 import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.SyndFeedOutput;
 import com.sun.syndication.io.XmlReader;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.xml.sax.InputSource;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.zip.GZIPInputStream;
-
 public class RSSManager {
-    URLConnection url;
+    URL url;
     SyndFeedInput syndFeedInput;
+    SyndFeedOutput syndFeedOutput;
     InputStream inputStream;
     SyndFeed syndFeed;
+    GuildMessageReceivedEvent event;
+    String feedString;
+    public void addFooter(SyndEntry entry){
+    }
+
+    private Object createFooter(String original, String link, String title) {
+        return 1;
+    }
+
     public RSSManager(String url, GuildMessageReceivedEvent event){
+        this.event= event;
         boolean ok = false;
         try {
-            this.url= new URL(url).openConnection();
+            this.url= new URL(url);
             inputStream = new URL(url).openConnection().getInputStream();
-            if("gzip".equals(this.url.getContentEncoding())){
-                inputStream = new GZIPInputStream(inputStream);
-            }
             syndFeedInput = new SyndFeedInput();
-            InputSource inputSource = new InputSource(inputStream);
-            syndFeed = syndFeedInput.build(inputSource);
+            syndFeed = syndFeedInput.build(new XmlReader(this.url));
             ok=true;
         }
         catch (Exception ex) {
             ex.printStackTrace();
             System.out.println("ERROR: "+ex.getMessage());
         }
+
         if(ok){
-            for(int i=0;i<5;i++){
-                event.getChannel().sendMessage((String)syndFeed.getLinks().get(i)).queue();
+
+            for (Object o : syndFeed.getEntries()) {
+
+                SyndEntry entry = (SyndEntry) o;
+                addFooter(entry);
+
             }
+            StringWriter writer = new StringWriter();
+
+            try {
+                syndFeedOutput.output(syndFeed, writer);
+            } catch (IOException | FeedException e) {
+                e.printStackTrace();
+            }
+
+            feedString=writer.toString();
         }
     }
 
