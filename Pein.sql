57a38afa55c087335173d4d80be2f471c2c4e2d0
Paul <gigi.boss52@gmail.com>
2021-05-30 15:44:14 +0300

    AddNews + categories + new db

--- Pein.sql
+++ Pein.sql
@@ -1,3 +1,20 @@
+-- Table: public.category
+
+-- DROP TABLE public.category;
+
+CREATE TABLE public.category
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    name character varying(32) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT category_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.category
+    OWNER to postgres;
+	
+	
 -- Table: public.feed
 
 -- DROP TABLE public.feed;
@@ -7,7 +24,6 @@ CREATE TABLE public.feed
     id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
     name character varying(32) COLLATE pg_catalog."default" NOT NULL,
     link character varying(256) COLLATE pg_catalog."default" NOT NULL,
-    category character varying(32) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT "Feed_pkey" PRIMARY KEY (id)
 )
 
@@ -17,5 +33,18 @@ ALTER TABLE public.feed
     OWNER to postgres;
 	
 	
-INSERT INTO public.feed( name, link, category)
-	VALUES( 'News', 'https://www.news.ro/rss', 'any');
+-- Table: public.feedcategory
+
+-- DROP TABLE public.feedcategory;
+
+CREATE TABLE public.feedcategory
+(
+    id_feed bigint NOT NULL,
+    id_category bigint NOT NULL,
+    CONSTRAINT feedcategory_pkey PRIMARY KEY (id_category, id_feed)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.feedcategory
+    OWNER to postgres;
